ec80c55931ad483b4074964058f5c4c516262a3f
Emanuele d'Ajello <emanuele@MacBook-Pro-di-Emanuele.local>
2022-11-23 12:34:39 +0100

    aggiunto verific su un percorso murato

--- problemi_homework2/problema1.cc
+++ problemi_homework2/problema1.cc
@@ -4,15 +4,23 @@
 
 //#include <bits/stdc++.h>
 
+
+/*
+Soluzione con backtracking
+*/
+
 using namespace std;
 
+//Struttura definita per identificare una posizione all'interno della matrice.
 typedef struct pos{
+
     int x,y;//x= riga, y = colonna
-    bool operator==(const struct pos &my){return (this->x==my.x && this->y == my.y);}
-    void print(){cout<<this->x<<","<<this->y;}
+    bool operator==(const struct pos &my){return (this->x==my.x && this->y == my.y);} //Overload dell operatore di uaguaglianza
+    void print(){cout<<this->x<<","<<this->y;}                                        //Funzione di comodo.
+
 }Pos;
 
-//stampa la boards nel caso di stampa post soluzioen fa vedere anche i -1 
+//stampa la boards nel caso di stampa post soluzioen fa vedere anche i valori "- 1" assegnati che percorrono il percorso trovato. 
 void print_boards(vector <vector<int> > &boards)
 {
     for (size_t i = 0; i < boards.size(); i++)
@@ -25,6 +33,7 @@ void print_boards(vector <vector<int> > &boards)
     }
 }
 
+//Stampa la soluzione trovata.
 void display_solution(vector <Pos> &solution)
 {
     cout<<"PATH = ";
@@ -53,7 +62,9 @@ bool check_0(vector < vector <int> > &visit,Pos current, vector <vector <int> > 
 }
 
 //funziona ricorsiva che prova tutti i percorsi .
-void backtracking(vector < vector <int>> &visit ,vector <Pos> &ending_solution, vector <Pos> &current_solution, Pos current_posizion, vector <vector <int> > &boards){
+//"main" del backtracking tenta ogni possibile strada fattibile finchè riesce a proseguire.
+//La complessità è esponenzia ed è dipendente da quanti vincoli ho sul percorso.
+void backtracking(vector < vector <int> > &visit ,vector <Pos> &ending_solution, vector <Pos> &current_solution, Pos current_posizion, vector <vector <int> > &boards){
 
     if(current_posizion.y == boards[0].size()-1)
     {
@@ -75,7 +86,11 @@ void backtracking(vector < vector <int>> &visit ,vector <Pos> &ending_solution, 
     else
     {  
         
-
+        /*-------------------------------------------------------------------------------------------------   
+        |    Qui verifico se la soluzione corrente è già maggiore si una soluzione trovata in precedenza,  |
+        |    in queso caso non c'è alcun motivo di continuare la ricerca,                                  |
+        |    quindi applico un "pruning dell'albero di ricorsione."                                        |
+        ---------------------------------------------------------------------------------------------------*/
         current_solution.push_back(current_posizion);
         if(ending_solution.empty()==false)
         {
@@ -83,15 +98,17 @@ void backtracking(vector < vector <int>> &visit ,vector <Pos> &ending_solution, 
                 return;            
         }
 
-
+        //La matrice visit mi tiene traccia dei percorsi già visitati.
         visit[current_posizion.x][current_posizion.y] = 1;
         
-        //display_solution(current_solution);
+        //Possibili nuovi spostamenti.
         Pos current_go_right = {.x = current_posizion.x, .y = current_posizion.y + 1};
         Pos current_go_down = {.x = current_posizion.x-1, .y = current_posizion.y};
         Pos current_go_up = {.x = current_posizion.x + 1, .y = current_posizion.y};
         Pos current_go_left = {.x = current_posizion.x, .y = current_posizion.y -1};
 
+        //Per ogni possibile nuova posizione sulla matrice eseguo prima un check sulla fattibilità dello spostamento,
+        //dopo questo richiamo ricorsivamente "backtraking" con il nuovo punto dato dalla mossa eseguita.
         if(check_0(visit,current_go_right,boards))
             backtracking(visit, ending_solution, current_solution,current_go_right,boards);
 
@@ -111,11 +128,13 @@ void backtracking(vector < vector <int>> &visit ,vector <Pos> &ending_solution, 
     }
 }
 
-//inserisco un due vicino agli 0 per facilitare la funzione di check choise
+//Processa una matrice data in quanto essendo anche i punti adiacenti agli "0" inaccessibili,
+//è coneventiente preprocessare la matrice con gli "0" inserendo dei "2" vicino a quest'ultimi,
+//così facendo la funzione di check per le posizione possibili risulta di immediata implementazione.
 vector <vector <int > > pre_processing_matrix(vector < vector <int> > &matrix)
 {
 
-    vector < vector <int>>new_matrix = matrix;
+    vector < vector <int> >new_matrix = matrix;
 
     for (size_t i = 0; i < new_matrix.size(); i++)
     {
@@ -154,6 +173,27 @@ vector <vector <int > > pre_processing_matrix(vector < vector <int> > &matrix)
 
 }
 
+//Per ridurre ulteriormente la complessità verifico se esiste almeno una soluzione,
+//tale verifica è effettuata considerando una colonna fissata verificare se esiste almeno un "1" altrimenti, 
+//c'è un muro di "0" o di "1" vicino agli zero.
+bool is_there_a_wall(vector <vector <int> > &matrix)
+{
+    
+    for (size_t i = 0; i < matrix[0].size(); i++)
+    {
+        int count = 0;
+        for (size_t j = 0; j < matrix.size(); j++)
+        {
+            if(matrix[j][i] ==0 || matrix[j][i]==2)count++;
+        }
+        if(count==matrix.size())return true; //ho trovato un muro
+    }
+    return false;
+}
+
+//La funzione find_min_path inizia già il backtracking, prunacompletamente dei rami se portano a percorsi senza soluzione,
+//o se non hanno punti di partenza.
+//Infine lancia per ogni punto di partenza disponibile il backtracking.
 vector <Pos> find_min_path(vector <vector <int> > &boards)
 {
 
@@ -168,7 +208,14 @@ vector <Pos> find_min_path(vector <vector <int> > &boards)
     cout<<endl;
     print_boards(max);
 
+
     vector <Pos> real_ending(2000);
+
+    if (is_there_a_wall(max)) 
+    {
+        real_ending.clear();
+        return real_ending;
+    }
     int i,j;
     for (i = 0; i < max.size(); i++)
         {
@@ -224,14 +271,10 @@ int main()
         int j = rand()%m;
         boards[i][j] = 0;
     }
-    cout<<endl;
+    
     print_boards(boards);
 
-/*
-    cout<< endl;
-    vector <vector <int> > new_matrix = pre_processing_matrix(boards);
-    print_boards(new_matrix, n, m);
-*/
+
     vector <Pos> solution = find_min_path(boards);
 
     display_solution(solution);
