8278f9784bc58d901d006c74af8c929b6b465b27
Emanuele d'Ajello <emanuele@MBP-di-Emanuele.lan>
2022-11-23 23:46:40 +0100

    new

--- problemi_homework2/problema2.cc
+++ problemi_homework2/problema2.cc
@@ -1,4 +1,4 @@
-//#include <bits/stdc++.h>
+//#include <bits/stdc++.h> "Questo non piace al clang che sta sul mac gcc sempre meglio!"
 #include <iostream>
 #include <unordered_map>
 #include <vector>
@@ -6,9 +6,10 @@
 using namespace std;
 
 /*
-Una volta sviluppata la ricorrenza dp(n) = (n-1)dp(n-2) + dp(n-1).
+Una volta ricavata la ricorrenza dp(n) = (n-1)dp(n-2) + dp(n-1).
 è immediato ricavare i casi base , d(1) e d(2) i quali hanno rispettivamente valore 1 e 2.
-Si utilizza una map per la memoization per tenere traccia del problema precedente ogni volte che esso 
+Si utilizza una map per la memoization per tenere traccia del problemi già risolti
+all indice del n-esimo problema stesso.
 */
 
 
@@ -27,9 +28,21 @@ int64_t dp(int n, unordered_map<int, int64_t> &mp)
 int main()
 {
     
-    unordered_map <int,long>a;
-    
-    cout<<dp(4,a)<<endl;
+    unordered_map <int,int64_t>a;
+    //casi di test
+    int T;
+    cout<<"Numero di casi di test: ";
+    cin>>T;
+    while(T--)
+    {   
+        int n;
+        cout<<"Inserire cardinalità insieme :";
+        cin>>n;
+        int64_t res = dp(n,a);
+        cout<<"risultato : "<<res<<endl;
+    }
+
+
 
     return 0;
 }
