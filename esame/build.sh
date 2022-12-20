84b6df7d30a9358fe30d3933fb83993905e84e3b
Emanuele d'Ajello <emanuele@MBP-di-Emanuele.lan>
2022-12-20 21:46:16 +0100

    newù

---
+++ esame/build.sh
@@ -0,0 +1,1 @@
+g++ -o ese esercizio.cc 
---
+++ esame/esercizio.cc
@@ -0,0 +1,35 @@
+#include <numeric>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+   /*
+    Si è utilizzato l'approccio di programmazione dinamica top-down + memoization.
+
+    Vengono quindi qui elencati i 5 punti:(n = lunghezza della stringa)
+
+    
+    1)  Definizione del sottoproblema :
+    
+    2)  Guessing:
+
+    3) La ricorrenza ricavata nel approccio è:
+
+    4) Il tempo totale è uguale a: #sottoproblemi*tempo-per-sottoproblema = 
+
+    5) Il problema originario si risolve utilizzando:
+    
+*/
+
+int main(int argc, char const *argv[])
+{
+    int T; //numero casi di test
+    cin>>T;
+    while(T--)
+    {
+        //fai cose
+    }
+
+    return 0;
+}
